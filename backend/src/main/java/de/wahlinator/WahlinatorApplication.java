cd067c83df678015d3ada5a602eb6a329d921a9b
Karlo Ivanovic <ivanovickarlo213@gmail.com>
2023-03-23 15:18:43 +0100

    Initial backend

---
+++ backend/src/main/java/de/wahlinator/WahlinatorApplication.java
@@ -0,0 +1,13 @@
+package de.wahlinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WahlinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WahlinatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/wahlinator/WahlinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wahlinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WahlinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
