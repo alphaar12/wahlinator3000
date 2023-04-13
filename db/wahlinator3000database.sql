fd63f15ea22708c6a112ee1089b810e13bff0a22
corvpauer <corvpauer@gmail.com>
2023-04-13 16:15:04 +0200

    Database

--- db/wahlinator3000database.sql
+++ db/wahlinator3000database.sql
@@ -1,14 +1,19 @@
+CREATE DEFINER=`21A_Kessler_inf21128`@`%` PROCEDURE `Wahlinator3000Database`()
+BEGIN
+
 START TRANSACTION;
 
+DROP TABLE IF EXISTS Has_Voted_Election;
+DROP TABLE IF EXISTS HasRole;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS HatRolle;
-DROP TABLE IF EXISTS HatGewaehlt;
-DROP TABLE IF EXISTS ParteiErgebnis;
-DROP TABLE IF EXISTS PolitikerErgebnis;
-DROP TABLE IF EXISTS Wahl;
-DROP TABLE IF EXISTS Partei;
-DROP TABLE IF EXISTS Politiker;
+DROP TABLE IF EXISTS Election_Political_Party;
+DROP TABLE IF EXISTS Election_Political_Member;
+DROP TABLE IF EXISTS Election_Political_Member_Votes;
+DROP TABLE IF EXISTS Election_Member_Party_Votes;
+DROP TABLE IF EXISTS Election;
+DROP TABLE IF EXISTS Political_Member;
+DROP TABLE IF EXISTS Political_Party;
 
 -- CREATE TABLES
 CREATE TABLE users (
@@ -26,56 +31,84 @@ CREATE TABLE roles (
     name VARCHAR(50)
 );
 
---    CREATE TABLE HatRolle (
---        AccountID INT,
---        FOREIGN KEY (AccountID)
---            REFERENCES users (ID),
---        RollenID INT,
---        FOREIGN KEY (RollenID)
---            REFERENCES roles (ID)
---    );
---
---    CREATE TABLE Wahl (
---        ID INT AUTO_INCREMENT PRIMARY KEY,
---        Typ INT,
---        StartDatum DATE,
---        EndDatum DATE
---    );
---
---    CREATE TABLE HatGewaehlt (
---        ID INT,
---        FOREIGN KEY (ID)
---            REFERENCES users (ID)
---    );
---
---    CREATE TABLE Partei (
---        ID INT AUTO_INCREMENT PRIMARY KEY,
---        ParteiName VARCHAR(50)
---    );
---
---    CREATE TABLE Politiker (
---        ID INT AUTO_INCREMENT PRIMARY KEY,
---        PolitikerName VARCHAR(50),
---        PolitikerVorname VARCHAR(50)
---    );
---
---    CREATE TABLE ParteiErgebnis (
---        ParteiID INT,
---        WahlID INT,
---        FOREIGN KEY (ParteiID)
---            REFERENCES Partei (ID),
---        FOREIGN KEY (WahlID)
---            REFERENCES Wahl (ID)
---    );
---
---    CREATE TABLE PolitikerErgebnis (
---        PolitikerID INT,
---        WahlID INT,
---        FOREIGN KEY (PolitikerID)
---            REFERENCES Politiker (ID),
---        FOREIGN KEY (WahlID)
---            REFERENCES Wahl (ID)
---    );
+    CREATE TABLE HasRole (
+        users_ID INT,
+        FOREIGN KEY (users_ID)
+            REFERENCES users (ID),
+        roles_ID INT,
+        FOREIGN KEY (roles_ID)
+            REFERENCES roles (ID)
+);
+
+	CREATE TABLE Election (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+		Type VARCHAR(50),
+        Region VARCHAR(50),
+        Votes INT,
+		Start_Date DATE,
+        End_start DATE
+);
+
+CREATE TABLE Political_Party (
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Election_Political_Party (
+		Election_ID INT,
+        FOREIGN KEY (Election_ID)
+            REFERENCES Election (ID),
+        Political_Party_ID INT,
+        FOREIGN KEY (Political_Party_ID)
+            REFERENCES Political_Party (ID)
+);
+
+CREATE TABLE Political_Member (
+		ID INT AUTO_INCREMENT PRIMARY KEY,
+        First_Name VARCHAR(50),
+        Last_Name VARCHAR(50),
+        Political_Party_ID INT,
+        FOREIGN KEY (Political_Party_ID)
+            REFERENCES Political_Party (ID)
+);
+
+CREATE TABLE Election_Political_Member (
+		Election_ID INT,
+        FOREIGN KEY (Election_ID)
+            REFERENCES Election (ID),
+        Political_Member_ID INT,
+        FOREIGN KEY (Political_Member_ID)
+            REFERENCES Political_Member (ID)
+);
+
+CREATE TABLE Has_Voted_Election (
+		Users_ID INT,
+        FOREIGN KEY (Users_ID)
+            REFERENCES users (ID),
+		Election_ID INT,
+        FOREIGN KEY (Election_ID)
+            REFERENCES Election (ID)
+);
+
+CREATE TABLE Election_Political_Member_Votes(
+		Election_ID INT,
+        FOREIGN KEY (Election_ID)
+            REFERENCES Election (ID),
+		Political_Member_ID INT,
+        FOREIGN KEY (Political_Member_ID)
+            REFERENCES Political_Member (ID),
+		Votes INT
+);
+
+CREATE TABLE Election_Member_Party_Votes(
+		Election_ID INT,
+        FOREIGN KEY (Election_ID)
+            REFERENCES Election (ID),
+		Political_Party_ID INT,
+        FOREIGN KEY (Political_Party_ID)
+            REFERENCES Political_Party (ID),
+		Votes INT
+);
 
 -- FILL DATA
 INSERT INTO roles(ID, name) VALUES(1, 'ROLE_USER');
@@ -87,3 +120,4 @@ VALUES 	(2147483647, "L28771V9V","Corvin","Pape",DATE '2000-05-20',27432,"Passwo
 
 --    INSERT INTO HatRolle
 --    VALUES (1,1);
+END
