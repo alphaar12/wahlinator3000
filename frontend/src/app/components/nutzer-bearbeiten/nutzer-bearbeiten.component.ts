5d566ccbd87cc380610a3e778c13f05bd6ccc0ef
Daniel Monserrat <daniel.monserrat@outlook.de>
2023-04-13 16:20:07 +0200

    daten für editUser von holen

--- frontend/src/app/components/nutzer-bearbeiten/nutzer-bearbeiten.component.ts
+++ frontend/src/app/components/nutzer-bearbeiten/nutzer-bearbeiten.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-nutzer-bearbeiten',
@@ -12,27 +13,45 @@ export class NutzerBearbeitenComponent {
 
 
   createForm: FormGroup;
+  showButton = false;
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+  public userDetails: any;
 
-  constructor(private userService: UserService, private formBuilder: FormBuilder, private router: Router) {
+  constructor(private userService: UserService, private formBuilder: FormBuilder, private router: Router, private snackBar: MatSnackBar) {
     this.createForm = this.formBuilder.group({
       personalNumber: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       birthdate: ['', Validators.required],
-      zipCode: ['', Validators.required]
+      zipCode: ['', Validators.required],
     });
   }
 
-  addUser(personalNumber: String, firstName: String, lastName: String, birthdate: any, zipCode: number) {
-    this.userService.addUser(personalNumber, firstName, lastName, birthdate, zipCode).subscribe(() => {
-      this.router.navigate(['/list']).then(r => {})
-    });
+  getUser(personalNumber: string) {
+    this.userService.getUserByPersonalNumber(personalNumber).subscribe(
+      (data: any) => {
+        this.userDetails = data;
+        console.log(this.userDetails);
+        this.setValue("firstName", this.userDetails.firstName);
+        this.setValue("lastName", this.userDetails.lastName);
+        this.setValue("firstName", this.userDetails.firstName);
+        this.setValue("birthdate", this.userDetails.birthdate);
+        this.setValue("zipCode", this.userDetails.zipCode);
+      }
+    );
+
+  }
+
+  setValue(id: string, value: string) {
+    const inputField = document.getElementById(id);
+    if (inputField) {
+      inputField.setAttribute("value", value);
+    }
   }
 
   ngOnInit(): void {
-
-
   }
-
 
 }
