514ad810e8b6c988a0d802f693753dbbd9111ed3
jenniii0 <117271577+jenniii0@users.noreply.github.com>
2023-05-04 15:42:32 +0200

    Added data region and date

--- frontend/src/app/components/landtagswahlsl/landtagswahlsl.component.ts
+++ frontend/src/app/components/landtagswahlsl/landtagswahlsl.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ElectionService} from "../../services/election/election.service";
+import {Observable, throwError} from "rxjs";
+import {catchError} from "rxjs/operators";
 
 @Component({
   selector: 'app-landtagswahlsl',
   templateUrl: './landtagswahlsl.component.html',
   styleUrls: ['./landtagswahlsl.component.css']
 })
-export class LandtagswahlslComponent {
+export class LandtagswahlslComponent implements OnInit {
+  public electionData1: any;
+  public electionData2: any;
+  public errorMessage: any;
 
+  constructor(private electionService: ElectionService) {
+  }
+
+  ngOnInit(): void {
+    this.getElection(1).subscribe(
+      (data) => {
+        console.log(data);
+        this.electionData1 = data;
+      },
+      (error) => {
+        this.errorMessage = error.error.message;
+        console.log(this.errorMessage);
+      }
+    );
+
+    this.getElection(2).subscribe(
+      (data) => {
+        console.log(data);
+        this.electionData2 = data;
+      },
+      (error) => {
+        this.errorMessage = error.error.message;
+        console.log(this.errorMessage);
+      }
+    );
+  }
+
+  getElection(electionId: number): Observable<any> {
+    return this.electionService
+      .getElection(electionId)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    console.error(error);
+    return throwError(error);
+  }
 }
+
