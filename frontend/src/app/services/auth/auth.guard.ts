853ffa23a6f49c68198fd512b6df1095ff67bdfb
Karlo Ivanovic <ivanovickarlo213@gmail.com>
2023-05-06 20:44:08 +0200

    Fix in frontend security

--- frontend/src/app/services/auth/auth.guard.ts
+++ frontend/src/app/services/auth/auth.guard.ts
@@ -15,10 +15,14 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     const isLoggedIn = this.storageService.isLoggedIn();
-    if (!isLoggedIn) {
+    const user = this.storageService.getUser();
+
+    if (isLoggedIn && user && user.roles && user.roles.includes('ROLE_ADMIN')) {
+      return true;
+    } else {
       console.log('Access denied, not logged in');
       this.router.navigate(['/login']);
+      return isLoggedIn;
     }
-    return isLoggedIn;
   }
 }
