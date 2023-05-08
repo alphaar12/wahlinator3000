a8fccd286e1c873a7966be4f3a0d03ef34332eb6
jenniii0 <117271577+jenniii0@users.noreply.github.com>
2023-05-08 12:04:21 +0200

    only one can be checked

--- frontend/src/app/components/bundestagswahl/bundestagswahl.component.ts
+++ frontend/src/app/components/bundestagswahl/bundestagswahl.component.ts
@@ -17,6 +17,27 @@ export class BundestagswahlComponent implements OnInit {
 
   constructor(private electionService: ElectionService) {
   }
+  
+  
+  function validateForm(): boolean {
+  const erststimme = document.getElementsByName("erststimme") as NodeListOf<HTMLInputElement>;
+  const zweitstimme = document.getElementsByName("zweitstimme") as NodeListOf<HTMLInputElement>;
+  if (!isChecked(erststimme) || !isChecked(zweitstimme)) {
+    alert("Bitte wählen Sie maximal eine Option aus dem jeweiligen Formular.");
+    return false;
+  }
+  return true;
+}
+
+function isChecked(option: NodeListOf<HTMLInputElement>): boolean {
+  for (let i = 0; i < option.length; i++) {
+    if (option[i].checked) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
   ngOnInit(): void {
     this.getElection(1).subscribe(
