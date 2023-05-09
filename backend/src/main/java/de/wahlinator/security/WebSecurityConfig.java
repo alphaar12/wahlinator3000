e40074fa3cc1c78d39fa2b1b89bb0ef1f2ca3126
Karlo Ivanovic <ivanovickarlo213@gmail.com>
2023-05-09 19:54:30 +0200

    Small backend fix

--- backend/src/main/java/de/wahlinator/security/WebSecurityConfig.java
+++ backend/src/main/java/de/wahlinator/security/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/elections/**").permitAll()
+                .antMatchers("/elections/vote/**").permitAll()
                 .antMatchers("/admin/**").permitAll()
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
